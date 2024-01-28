e84b7ea87f6d7917f2399f83e1c55e2a4d5819e9
Christian <chrisdsmith0517@gmail.com>
2024-01-28 14:16:12 -0500

    Solved

--- src/main/java/SkillBuilder1.java
+++ src/main/java/SkillBuilder1.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 /**
  * Skill Builder 0
@@ -9,7 +10,21 @@ import java.util.Scanner;
 public class SkillBuilder1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        //TODO : Replace this comment with your code
+        String name = " ";
+        double spice = 0.0;
+        double spiceCalc = 0.0;
+        double innerSpice = 0.0;
+        System.out.println("You're Jane's friend!");
+        System.out.println("What's your name?");
+        name = input.nextLine();
+        System.out.println("Enter a floating-point number:");
+        spice = input.nextDouble();
+        innerSpice = Math.sqrt(5)/Math.pow(spice,3.0);
+        spiceCalc = (4.0/3) *Math.pow(2.0,innerSpice);
+        System.out.println("Well "+name +","+" the spice value resulted in "+ spiceCalc);
+        double convertedCalc = (int)(spiceCalc * 100)/100.0;
+        System.out.println("And the converted value is " + convertedCalc);
+
     }
 
     public static void calcWallPaint() {
@@ -27,9 +42,18 @@ public class SkillBuilder1 {
         double wallWidth = input.nextDouble();
 
         // TODO: Calculate and output the wall's area
-
+        double wallArea = wallWidth * wallHeight;
+        System.out.println("Wall area: "+ wallArea +" square feet");
         // TODO: Calculate and output the amount of paint in gallons needed to paint the wall
+        double paintNeed = gallonsPerCan/(squareFeetPerGallons/wallArea);
+        double convertedCalc = (int)(paintNeed * 100)/100.00;
+        System.out.print("Paint needed: ");
+        System.out.printf("%.2f",convertedCalc);
+        System.out.print(" gallons\n");
+
 
         // TODO: Calculate and output the number of 1 gallon cans needed to paint the wall, rounded up to nearest integer
+        double cans = Math.ceil(convertedCalc);
+        System.out.println("Cans needed: " + (int)cans + " can(s)");
     }
 }
